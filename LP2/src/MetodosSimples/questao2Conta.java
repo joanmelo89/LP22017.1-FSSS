966a3e6c359d9161dd5a71f3585e4e4b62c54f47
joanmelo89 <joanmelo89@gmail.com>
2017-05-08 20:08:21 -0300

    Atualização
    Classes novas

--- LP2/src/MetodosSimples/questao2Conta.java
+++ LP2/src/MetodosSimples/questao2Conta.java
@@ -10,51 +10,52 @@ package MetodosSimples;
  * @author joan-
  */
 public class questao2Conta {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         ContaCorrente conta = new ContaCorrente();
         conta.numero = "123456";
         conta.agencia = "1234";
         conta.especial = true;
         conta.limiteEspecial = 500;
-        conta.saldo = -10;
-        
+        conta.saldo = 1000;
+
         System.out.println("Saldo da conta " + conta.numero + " = " + conta.saldo);
-        
+
         boolean saqueEfetuado = conta.realizarSaque(10);
-        
-        if (saqueEfetuado){
-           System.out.println("Saque efetuado com sucesso"); 
-           conta.consultarSaldo();
+
+        if (saqueEfetuado) {
+            System.out.println("Saque efetuado com sucesso");
+            conta.consultarSaldo();
         } else {
-            System.out.println("Não foi possível realizar saque. Saldo insuficiente"); 
+            System.out.println("Não foi possível realizar saque. Saldo insuficiente");
         }
-        
+
         saqueEfetuado = conta.realizarSaque(500);
-        System.out.println("Tentativa de saque de 500 reais"); 
-        if (saqueEfetuado){
-           System.out.println("Saque efetuado com sucesso"); 
-           conta.consultarSaldo();
+        System.out.println("Tentativa de saque de 500 reais");
+        if (saqueEfetuado) {
+            System.out.println("Saque efetuado com sucesso");
+            conta.consultarSaldo();
         } else {
-            System.out.println("Não foi possível realizar saque. Saldo insuficiente"); 
+            System.out.println("Não foi possível realizar saque. Saldo insuficiente");
         }
-        
-        System.out.println("Depósito de 500 reais"); 
+
+        System.out.println("Depósito de 500 reais");
         conta.depositar(500);
         conta.consultarSaldo();
-        
-        if (conta.verificarUsoChequeEspecial()){
-            System.out.println("Está usando cheque especial"); 
+
+        if (conta.verificarUsoChequeEspecial()) {
+            System.out.println("Está usando cheque especial");
         } else {
-            System.out.println("Não está usando cheque especial"); 
+            System.out.println("Não está usando cheque especial");
         }
-        
+
         conta.realizarSaque(600);
         conta.consultarSaldo();
-        if (conta.verificarUsoChequeEspecial()){
-            System.out.println("Está usando cheque especial"); 
+        if (conta.verificarUsoChequeEspecial()) {
+            System.out.println("Está usando cheque especial");
         } else {
-            System.out.println("Não está usando cheque especial"); 
+            System.out.println("Não está usando cheque especial");
         }
     }
 }
